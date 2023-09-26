867d260e22ab362c9dea2e0c13f4c378f6f8c546
Ami Fischman <ami@fischman.org>
2023-09-26 14:30:20 -0700

    Speed up reaction time by keying off of TYPE_VIEW_SELECTED instead of TYPE_VIEW_CLICKED, and added throttling to compensate for double-delivery

--- app/src/main/java/org/fischman/noexplore/NoExploreService.java
+++ app/src/main/java/org/fischman/noexplore/NoExploreService.java
@@ -1,10 +1,16 @@
 package org.fischman.noexplore;
 
 import android.accessibilityservice.AccessibilityService;
+import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 
 public class NoExploreService extends AccessibilityService {
+    // Time (epoch millis) of the last event that was acted on.
+    // Used to throttle actions, in preference to a longer android:notificationTimeout value because
+    // that also slows down the first action instead of only subsequent ones.
+    private long lastEventTime = 0;
+
     public NoExploreService() {}
 
     private String emptyIfNull(CharSequence s) {  return s != null ? s.toString() : ""; }
@@ -24,22 +30,30 @@ public class NoExploreService extends AccessibilityService {
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
+        long eventTime = event.getEventTime();
+        if (eventTime - lastEventTime < 500) { return; }
         if (!"com.instagram.android".equals(event.getPackageName())) {
             // Should be excluded by XML configuration!
             throw new RuntimeException("Unexpected event package name: " + event);
         }
         int eventType = event.getEventType();
-        if (eventType == AccessibilityEvent.TYPE_VIEW_CLICKED) {
+        // TYPE_VIEW_SELECTED is delivered a bit faster than TYPE_VIEW_CLICK so use that, but also
+        // is delivered multiple times, hence the eventTime-based throttling.
+        if (eventType == AccessibilityEvent.TYPE_VIEW_SELECTED) {
             CharSequence description = event.getContentDescription();
             if ("Search and explore".equals(description) || "Reels".equals(description)) {
+                lastEventTime = eventTime;
                 performGlobalAction(GLOBAL_ACTION_BACK);
-                return;
             }
+            return;
         }
+        if (eventType != AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED) return;
         AccessibilityNodeInfo source = event.getSource();
         if (source == null) { return; }
-        if (containsRecursively(source, "Suggested Posts"))
+        if (containsRecursively(source, "Suggested Posts")) {
+            lastEventTime = eventTime;
             performGlobalAction(GESTURE_SWIPE_UP);
+        }
     }
 
     @Override
