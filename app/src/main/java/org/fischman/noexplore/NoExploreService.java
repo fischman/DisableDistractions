c95550ab7d9d43f5a91a9ea1b329221ec2775f96
Ami Fischman <ami@fischman.org>
2023-10-01 18:10:05 -0700

    Only swipe up for Suggested Posts on scroll to avoid unusability if run out of posts. Also added some debugging helpers

--- app/src/main/java/org/fischman/noexplore/NoExploreService.java
+++ app/src/main/java/org/fischman/noexplore/NoExploreService.java
@@ -1,11 +1,18 @@
 package org.fischman.noexplore;
 
 import android.accessibilityservice.AccessibilityService;
+import android.graphics.Rect;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 
 public class NoExploreService extends AccessibilityService {
+    private final boolean DEBUG = false;
+
+    private void emit(String msg) {
+        Log.e("AMI", msg);
+    }
+
     // Time (epoch millis) of the last event that was acted on.
     // Used to throttle actions, in preference to a longer android:notificationTimeout value because
     // that also slows down the first action instead of only subsequent ones.
@@ -28,6 +35,16 @@ public class NoExploreService extends AccessibilityService {
         return false;
     }
 
+    // Handy for debugging.
+    private void dumpRecursively(AccessibilityNodeInfo node) {
+        if (!DEBUG || node == null) return;
+        emit(node.getText() + " - " + node.getHintText() + " - " + node.getTooltipText());
+        int count = node.getChildCount();
+        for (int i = 0; i < count; ++i) {
+            dumpRecursively(node.getChild(i));
+        }
+    }
+
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
         long eventTime = event.getEventTime();
@@ -36,6 +53,7 @@ public class NoExploreService extends AccessibilityService {
             // Should be excluded by XML configuration!
             throw new RuntimeException("Unexpected event package name: " + event);
         }
+        if (DEBUG) emit("event is " + event);
         int eventType = event.getEventType();
         // TYPE_VIEW_SELECTED is delivered a bit faster than TYPE_VIEW_CLICK so use that, but also
         // is delivered multiple times, hence the eventTime-based throttling.
@@ -43,6 +61,7 @@ public class NoExploreService extends AccessibilityService {
             CharSequence description = event.getContentDescription();
             if ("Search and explore".equals(description) || "Reels".equals(description)) {
                 lastEventTime = eventTime;
+                if (DEBUG) emit("BACK because description is " + description);
                 performGlobalAction(GLOBAL_ACTION_BACK);
             }
             return;
@@ -50,8 +69,12 @@ public class NoExploreService extends AccessibilityService {
         if (eventType != AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED) return;
         AccessibilityNodeInfo source = event.getSource();
         if (source == null) { return; }
+        if (source.getActionList().stream().filter(
+                a -> a.getId() == AccessibilityNodeInfo.AccessibilityAction.ACTION_SCROLL_FORWARD.getId()).count() == 0) return;
         if (containsRecursively(source, "Suggested Posts")) {
             lastEventTime = eventTime;
+            if (DEBUG) emit("UP because saw Suggested Posts in source, which follows after source: " + source);
+            if (DEBUG) dumpRecursively(source);
             performGlobalAction(GESTURE_SWIPE_UP);
         }
     }
